380dd68f45010b7701b89b5fd195ee7601d832d7
Andre Bach <nonnormalizable@gmail.com>
2013-02-03 20:55:31 -0800

    Searching for a walking route works. Clean up.

--- app.py
+++ app.py
@@ -1,43 +1,29 @@
 #!/usr/bin/env python
 
 from flask import Flask, url_for, request, render_template
+from flaskext.mysql import MySQL
+
 app = Flask(__name__)
+app.config.update(
+    MYSQL_DATABASE_HOST = 'localhost',
+    MYSQL_DATABASE_PORT = 3306,
+    MYSQL_DATABASE_USER = 'bacha',
+    MYSQL_DATABASE_PASSWORD = 'mysqlPW',
+    MYSQL_DATABASE_DB = 'crime'
+    )
 
 @app.route('/')
 def index():
+    c = mysql.get_db().cursor()
+    c.execute("SELECT * FROM crime_raw LIMIT 9;")
+    print c.fetchone()
+
     return render_template('index.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    abort(404)
-    if request.method == 'POST':
-        return 'post method '+request.args.get('bob', '')
-    else:
-        return 'get method '+request.args.get('bob', '')
-
-@app.route('/user/<username>')
-def profile(username):
-    return 'User %s totally exists!' % username
-
-@app.route('/map_two')
-def map_two():
-    return """
-<head>
-  <script type="text/javascript"
-          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDXluoQqUGT51AesCSg-voAfkLWr8V36SQ&sensor=false">
-  </script>
-  <script type="text/javascript" src="../static/js/map_display.js"></script>
-</head>
-<body onload="initialize_map()">
-  <div id="map_canvas" style="width:100%; height:100%">
-</body>
-"""
-
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     app.debug = True
+
+    mysql = MySQL()
+    mysql.init_app(app)
+
     app.run()
--- static/js/map_display.js
+++ static/js/map_display.js
@@ -1,9 +1,51 @@
-function initialize() {
+var map;
+var directionsService = new google.maps.DirectionsService();
+var directionsDisplay;
+
+var testLoc = new google.maps.LatLng(37.84200, -122.26245);
+var marker = new google.maps.Marker({
+    position: testLoc,
+    title: "Hello marker!"
+});
+
+function initialize()
+{
+    directionsDisplay = new google.maps.DirectionsRenderer();
     var mapOptions = {
         center: new google.maps.LatLng(37.835, -122.263),
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("map_canvas"),
-				  mapOptions);
+    map = new google.maps.Map(document.getElementById("map_canvas"),
+			      mapOptions);
+    directionsDisplay.setMap(map);
+    marker.setMap(map);
+}
+
+function calcRoute()
+{
+    var start = document.getElementById("start").value;
+    var end = document.getElementById("end").value;
+    var request = {
+	origin:start,
+	destination:end,
+	travelMode: google.maps.TravelMode.WALKING,
+	provideRouteAlternatives: true
+    }
+    directionsService.route(request, function(result, status)
+			    {
+				if (status == google.maps.DirectionsStatus.OK) {
+				    directionsDisplay.setDirections(result);
+				}
+				lookAtResult(result);
+			    });
+}
+
+function lookAtResult(directionResult)
+{
+    for (i=0; i<directionResult.routes.length; i++)
+    {
+	console.log(i)
+	//console.log(directionResult.routes[i])
+    }
 }
